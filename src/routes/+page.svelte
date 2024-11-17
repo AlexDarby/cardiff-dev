20b39f733d9cec2e086fae6534d4ba48a0b85f34
Alex <alexjdarby@gmail.com>
2024-11-17 20:24:53 +0000

    initial with logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import img from '$lib/assets/logo_transparent.png';
+    const discord_link = 'https://discord.gg/83xdnFmHPd';
+</script>
+
+<div class="flex items-center justify-center h-screen bg-gray-100">
+    <a href="{discord_link}"><img src="{img}" class="max-w-full max-h-full sm:max-w-sm md:max-w-md lg:max-w-lg" alt="Logo for Cardiff Dev QR code."/></a>
+</div>
